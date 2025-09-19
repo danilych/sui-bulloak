d381c9a35a1a83e00fc98da99d9c7daed077e67d
danilych <danilych45@proton.me>
2025-09-19 17:19:41 +0200

    feat: use clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod scaffold;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "sui-bulloak", version, about = "A scaffolding tool for generating Move specification files to speed up formal verification on Sui with sui-prover.")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Scaffold {
+        #[arg(short, long)]
+        path: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Scaffold { path } => {
+            scaffold::scaffold(path);
+        }
+    }
 }
---
+++ src/scaffold.rs
@@ -0,0 +1,3 @@
+pub fn scaffold(path: String) {
+    println!("Scaffolding {}", path);
+}
